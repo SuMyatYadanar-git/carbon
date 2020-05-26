c6a52dc1b71d58f6dba8e910d6037cacdaccb996
Kumo <hsumyatmon.ntu@gmail.com>
2020-05-26 12:41:45 +0630

    db-change to kumo

--- db/carbonoffset_db.js
+++ db/carbonoffset_db.js
@@ -4,21 +4,21 @@ const bcrypt = require('bcrypt')
 const saltRounds = 10;
 
 // mysql.createConnection
-const con1 = mysql.createPool({
-  host: "localhost",
-   user: "root",
-  //  user: "user125",
-  password: "root",
-  database: "carbon_offset_db",
-  waitForConnections: true,
-});
-// const con1 = mysql.createConnection({
+// const con1 = mysql.createPool({
 //   host: "localhost",
-//   user: "kumo99",
+//    user: "root",
+//   //  user: "user125",
 //   password: "root",
 //   database: "carbon_offset_db",
 //   waitForConnections: true,
 // });
+const con1 = mysql.createConnection({
+  host: "localhost",
+  user: "kumo99",
+  password: "root",
+  database: "carbon_offset_db",
+  waitForConnections: true,
+});
 const con3 = mysql.createPool({
   host: "114.32.125.70",
   port: "33061",
